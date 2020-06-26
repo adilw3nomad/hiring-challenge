88f55c64eb1d5ece17f4a787cd9456b3f81f26ba
adilw3nomad <adiln1993@googlemail.com>
2020-06-26 23:04:31 +0100

    Refactors background#index tests, adds test to respond to XML

--- test/integration/backgrounds_test.rb
+++ test/integration/backgrounds_test.rb
@@ -2,20 +2,28 @@ require 'test_helper'
 
 class BackgroundsTest < ActionDispatch::IntegrationTest
   setup do
-    10.times{ create(:background, created_at: 2.days.ago)}
-  end
-
-  test '#index should return all backgrounds' do 
-    get backgrounds_path
-    assert_response :success
-    json = JSON.parse(response.body)
-    assert_equal 10, json['backgrounds'].size
-  end
-
-  test '#index should return the newest backgrounds first' do
+    9.times{ create(:background, created_at: 2.days.ago)}
     @latest_background = create(:background, created_at: 5.minutes.ago)
-    get backgrounds_path
-    json = JSON.parse(response.body)
-    assert_equal @latest_background.id, json['backgrounds'].first['id']
+  end
+  class IndexActionTest < BackgroundsTest
+    test 'it should return all backgrounds' do
+      get backgrounds_path as: :json
+      assert_response :success
+      json = JSON.parse(response.body)
+      assert_equal 10, json['backgrounds'].size
+    end
+
+    test 'it should return the newest backgrounds first' do
+      get backgrounds_path as: :json
+      json = JSON.parse(response.body)
+      assert_equal @latest_background.id, json['backgrounds'].first['id']
+    end
+
+    test 'it should respond to xml requests' do
+      get backgrounds_path as: :xml
+      assert_response :success
+      assert_equal 'application/xml', response.media_type
+      assert_equal @latest_background.attributes.to_xml, response.body.first
+    end
   end
 end
